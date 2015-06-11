75104150204d10a0b6b3aedda165d969378b6603
Xavi Lozano Segarra <xavils@Xavis-MacBook-Air.local>
2015-06-11 12:21:22 +0800

    best style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,5 +17,5 @@
 //= require angular-route/angular-route
 //= require angular-utils-disqus/dirDisqus
 //= require angular-bootstrap/ui-bootstrap
-//= require angucompelte/angucomplete
+//= require angucomplete/angucomplete
 //= require_tree .
